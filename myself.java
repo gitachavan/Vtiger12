bee88bfb60b53776e1b8cbab9e39a656d02e82bf
gitachavan <126804942+gitachavan@users.noreply.github.com>
2023-03-30 11:12:29 +0530

    Add files via upload

---
+++ myself.java
@@ -0,0 +1,14 @@
+class myself
+{
+public static void main(String[]args)
+{
+System.out.println("Name-Gitanjali Vasant Chavan");
+System.out.println("Branch-Civil Engineering");
+System.out.println("College Name-DR Daultrao aher college of engineering karad");
+System.out.println("Email Address-gitanjalichavan27@gmail.com");
+System.out.println("Address-Empire Eternity katraj pune");
+System.out.println("yop-2021");
+System.out.println("percentage-91.50");
+System.out.println("Academic project-Application of RS And GIS for geohydrological study of kolamba river western Maharashtra");
+}
+}
