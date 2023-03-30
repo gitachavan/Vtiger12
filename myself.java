04f29119ae133abeeb73a2b18b2dd5930f51566d
gitachavan <126804942+gitachavan@users.noreply.github.com>
2023-03-30 12:36:02 +0530

    Update myself.java

--- myself.java
+++ myself.java
@@ -7,6 +7,16 @@ System.out.println("Branch-Civil Engineering");
 System.out.println("College Name-DR Daultrao aher college of engineering karad");
 System.out.println("Email Address-gitanjalichavan27@gmail.com");
 System.out.println("Address-Empire Eternity katraj pune");
+System.out.println("Name-Gitanjali Vasant Chavan");
+System.out.println("Branch-Civil Engineering");
+System.out.println("College Name-DR Daultrao aher college of engineering karad");
+System.out.println("Email Address-gitanjalichavan27@gmail.com");
+System.out.println("Address-Empire Eternity katraj pune");
+System.out.println("Name-Gitanjali Vasant Chavan");
+System.out.println("Branch-Civil Engineering");
+System.out.println("College Name-DR Daultrao aher college of engineering karad");
+System.out.println("Email Address-gitanjalichavan27@gmail.com");
+System.out.println("Address-Empire Eternity katraj pune");
 
 
 System.out.println("Academic project-Application of RS And GIS for geohydrological study of kolamba river western Maharashtra");
