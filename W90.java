d6268977f9c98149437fb0e22bc763489771b7ad
gitachavan <126804942+gitachavan@users.noreply.github.com>
2023-03-31 09:00:33 +0530

    Add files via upload

---
+++ W90.java
@@ -0,0 +1,16 @@
+class W90
+{
+	public static void main(String[] args) 
+	{
+	 
+	 
+	 float f1=57.89f;
+	 float f2=89.90f;
+	 float ans=f1%f2;
+
+        
+        
+        System.out.println(ans);        
+        
+	}
+}
